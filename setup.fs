83206b6bc621a9ff6f222d622689ce3d157b4681
Sean Hayes <sean@dynamic-solutions.com>
2017-06-27 10:42:18 -0700

    Remove legacy dev tree folders

--- setup.fs
+++ setup.fs
@@ -12,13 +12,12 @@ fi
 #   /dev/ - development tree
 #   /tmp/ - user temp dir
 mkdir -p $ROOTDIR/{bin,dev,tmp}
-mkdir -p $ROOTDIR/dev/{conf,data,env,src,utilities,web}
+mkdir -p $ROOTDIR/dev/{conf,data,env,src}
 mkdir -p $ROOTDIR/dev/lib/{R,python}
 
 #change ACL's
-chown -R $USER:$(grep -q "^admin:" /etc/group && echo "admin" || echo "root") $ROOTDIR/{bin,dev,tmp}
+# chown -R $USER:$(grep -q "^admin:" /etc/group && echo "admin" || echo "root") $ROOTDIR/{bin,dev,tmp}
 
 chmod -R 755 $ROOTDIR/bin
 chmod -R 755 $ROOTDIR/{dev,tmp}
 chmod -R 700 $ROOTDIR/dev/conf
-
